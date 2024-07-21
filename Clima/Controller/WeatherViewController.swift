c8c9f273fac8adba8a4ab5f4bb0f5413dcf497f9
Asad Aftab <asadaftab.tuwien@gmail.com>
2024-07-21 17:48:48 +0200

    Dark Mode and Working with Vector Assets

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Asad Aftab on 21/07/2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+        
+    }
+
+
+}
+
